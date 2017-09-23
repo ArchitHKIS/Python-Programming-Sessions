430264b631da0f968b97ecd1d7d5c67b159f5a5f
“Archit <190902@hkis.edu.hk>
2017-09-23 16:13:12 +0800

    Clean Version
    Removed unnecessary variables.

--- PyCalc.py
+++ PyCalc.py
@@ -6,8 +6,6 @@ operator = input("Please enter 1 for Addition, 2 for Subtraction, 3 for Multipli
                  "\nand 5 for exponential multiplications\n")
 num1 = input("First number: ")
 num2 = input("Second number: ")
-something = 1
-somethingElse = 2
 
 if operator == 1:
     # remember to use == for comparing two different variables
@@ -20,7 +18,4 @@ if operator == 4:
     print num1/num2
 if operator == 5:
     print num1**num2
-    # the ** operator is the power function which is essentially n^n.
-
-if something == somethingElse:
-    print "Hello!"
+    # the ** operator is the power function which means it does: n^n.
