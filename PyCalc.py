e626bb6c4687e5f20dcaed4ef01a35bf08186075
“Archit <190902@hkis.edu.hk>
2017-10-16 19:08:19 +0800

    changed to fit python 3.6.3
    updated to fit syntax of 3.6.3.

--- PyCalc.py
+++ PyCalc.py
@@ -1,22 +1,22 @@
 # calculator made with basic methods using Python
 
-print "Welcome to PyCalc"
+print("Welcome to PyCalc")
     # greeting
-operator = input("Please enter 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division"
-                 "\nand 5 for exponential multiplications\n")
-num1 = input("First number: ")
-num2 = input("Second number: ")
+operator = eval(input("Please enter 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division"
+                 "\nand 5 for exponential multiplications\n"))
+num1 = eval(input("First number: "))
+num2 = eval(input("Second number: "))
 
 if operator == 1:
     # remember to use == for comparing two different variables
-    print num1+num2
+    print(num1+num2)
 if operator == 2:
-    print num1-num2
+    print(num1-num2)
 if operator == 3:
-    print num1*num2
+    print(num1*num2)
 if operator == 4:
-    print float(num1)/num2
+    print(float(num1)/num2)
     # float(int) is called casting. We will go over this in the next session.
 if operator == 5:
-    print num1**num2
+    print(num1**num2)
     # the ** operator is the power function which means it does: n^n.
